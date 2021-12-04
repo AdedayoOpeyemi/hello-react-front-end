c41fad5e24c8d4d34b79bb678d221c0b6b01741e
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-12-04 03:44:46 +0100

    added redux and connect to api

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,7 +1,15 @@
 import React from 'react';
+// import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchApiData } from '../redux/reducer';
 
 const Greeting = () => {
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(fetchApiData());
+  // }, []);
   const message = useSelector((state) => state.message);
 
   return (
